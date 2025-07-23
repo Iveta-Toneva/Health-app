dafad1c8178275a25e497db324fb24eab671599e
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-07-23 11:41:21 +0100

    Update CalorieCalculator.jsx

--- src/components/CalorieCalculator/CalorieCalculator.jsx
+++ src/components/CalorieCalculator/CalorieCalculator.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
-import { Button, CardHeader, Card, Radio, FormLabel, RadioGroup, FormControlLabel } from '@mui/material';
+import { Button, CardHeader, Card, Radio, FormLabel, RadioGroup, FormControlLabel,Typography } from '@mui/material';
 
 
 export default function FormPropsTextFields() {
@@ -15,13 +15,53 @@ export default function FormPropsTextFields() {
 
     });
 
+    const [bmr, setBmr] = React.useState(0);
+    const [calories, setCalories] = React.useState(0);
+
     const onChangeHandler = (e) => {
         setValues(state => ({ ...state, [e.target.name]: e.target.value }));
     }
 
     const calculate = () => {
         const { gender, age, height, weight, activity } = values;
-        console.log('User Input:', values);
+
+        const ageValue = Number(age);
+        const heightaValue = Number(height);
+        const weightValue = Number(weight);
+
+        let bmrValue = 0;
+        let caloriesValue = 0;
+        if (gender === 'male') {
+            bmrValue = 10 * weightValue + 6.25 * heightaValue - 5 * ageValue + 5;
+        } else if (gender === 'female') {
+            bmrValue = 10 * weightValue + 6.25 * heightaValue - 5 * ageValue - 161;
+        }
+        switch (activity) {
+            case 'BMR':
+                caloriesValue = bmrValue;
+                break;
+            case 'Sedentary':
+                caloriesValue = bmrValue * 1.2;
+                break;
+            case 'Lightly active':
+                caloriesValue = bmrValue * 1.375;
+                break;
+            case 'Moderately active':
+                caloriesValue = bmrValue * 1.55;
+                break;
+            case 'Very active':
+                caloriesValue = bmrValue * 1.725;
+                break;
+            case 'Extra active':
+                caloriesValue = bmrValue * 1.9;
+                break;
+            default:
+                break;
+        }
+
+        setBmr(bmrValue);
+        setCalories(caloriesValue);
+        console.log(calories)
     }
 
     return (
@@ -53,9 +93,13 @@ export default function FormPropsTextFields() {
                 <FormControlLabel value="Moderately active" control={<Radio />} label="Moderately active: Moderate exercise/sports 3-5 days/week." />
                 <FormControlLabel value="Very active" control={<Radio />} label="Very active: Hard exercise/sports 6-7 days a week." />
                 <FormControlLabel value="Extra active" control={<Radio />} label="Extra active: Very hard exercise/sports & a physical job." />
-                <FormControlLabel value="Professional athlete" control={<Radio />} label="Professional athlete." />
             </RadioGroup>
             <Button variant="contained" onClick={calculate}>Calculate</Button>
+            <div style={{ padding: '1rem' }}>
+                <Typography variant="h6"><strong>BMR:</strong> {bmr.toFixed(2)} kcal/day</Typography>
+                <Typography variant="h6"><strong>Calories:</strong> {calories.toFixed(2)} kcal/day</Typography>
+            </div>
         </Card>
+
     );
 }
