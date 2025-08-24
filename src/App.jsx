8fd0a0b792a424e4d779e613376c8c09b8d72246
Iveta-Toneva <iveta.v.toneva@gmail.com>
2025-08-24 09:25:55 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import ResponsiveAppBar from './components/Navigation/Navigation'
 import CalorieCalculator from './components/CalorieCalculator/CalorieCalculator'
+import Home from './components/Home/Home'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <ResponsiveAppBar />
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/calorieCalculator' element={<CalorieCalculator />} />
       </Routes>
     </>
